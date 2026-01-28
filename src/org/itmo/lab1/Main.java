ce264c18064a294171429c33bf11308d37cb0013
Sofia Taskina <sofiya.9691@gmail.com>
2026-01-28 21:35:02 +0300

    lab1

---
+++ src/org/itmo/lab1/Main.java
@@ -0,0 +1,16 @@
+package org.itmo.lab1;
+
+public class Main {
+    public static void main(String[] args)
+    {
+
+        Solution1 sol = new Solution1();
+
+        sol.print();
+        sol.task2();
+        sol.task3();
+        sol.task4();
+        sol.task5();
+        sol.task6();
+    }
+}
