a425c23aab135106cb8100cef4bfc28b547d571e
Sofia Taskina <sofiya.9691@gmail.com>
2026-01-28 21:48:59 +0300

    lab2

---
+++ src/org/itmo/lab2/Book.java
@@ -0,0 +1,50 @@
+package org.itmo.lab2;
+
+public class Book {
+    private String author;
+    private String title;
+    private Integer pages;
+    private Double price;
+    private Boolean isNew;
+
+    public Book() //конструктор без параметров
+    {
+    }
+    public Book(String author, String title, Integer pages, Double price, Boolean isNew)
+    {
+        this.author = author;
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
